5007a17a452b048a1c5549ff016093439fb2c41f
richy2305341355-png <richy2305341355@diu.edu.bd>
2026-04-17 09:50:03 +0600

    Update app.py

--- app.py
+++ app.py
@@ -759,4 +759,5 @@ def admin_delete_workout(id):
 #  RUN
 # ==============================================================
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 5000)) 
+    app.run(host='0.0.0.0', port=port)
